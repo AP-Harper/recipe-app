31c9f7fccb36cc3eb1b9e0bfc1def16603988b1c
AP-Harper <142895139+AP-Harper@users.noreply.github.com>
2024-05-08 18:21:32 +0100

    Added testing

---
+++ src/test/java/andrew/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package andrew.recipe.services;
+
+import andrew.recipe.domain.Recipe;
+import andrew.recipe.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
