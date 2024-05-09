a14b8b9b91dd8b2775d702db0875d89eecd11545
AP-Harper <142895139+AP-Harper@users.noreply.github.com>
2024-05-09 07:17:44 +0100

    Added Mock MVC and Spring Integration tests

--- src/test/java/andrew/recipe/controllers/IndexControllerTest.java
+++ src/test/java/andrew/recipe/controllers/IndexControllerTest.java
@@ -1,20 +1,31 @@
 package andrew.recipe.controllers;
 
-import andrew.recipe.repositories.RecipeRepository;
+import andrew.recipe.domain.Recipe;
 import andrew.recipe.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
+
+    @Mock
+    HashSet<Recipe> recipes;
     @Mock
     RecipeService recipeService;
     @Mock
@@ -26,18 +37,33 @@ class IndexControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         controller = new IndexController(recipeService);
+    }
 
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.view().name("index"));
     }
 
     @Test
     void getIndexPage() {
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
 
         String viewName = controller.getIndexPage(model);
 
         assertEquals("index", viewName);
-
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), eq(recipes));
+        assertEquals(2, recipes.size());
     }
 }
